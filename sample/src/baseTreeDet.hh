2d505433784b2f28c8880e34bcfa599796941504
cvson <cvson@utexas.edu>
2017-05-15 15:47:11 +0900

    Update baseTreeDet.hh

--- sample/src/baseTreeDet.hh
+++ sample/src/baseTreeDet.hh
@@ -574,7 +574,7 @@ Int_t baseTreeDet::CutSamplebyID(Long64_t entry, Int_t myid)
     
     //signal region
     if (myid==0){
-        if (CutSignalR(entry)>0) return 1;
+        if (CutPreselection(entry)>0 && pidfsi>0.08 && mumucl>0.6 && pmucl>0.25) return 1;
         else return -1;
     }
     //CR1
@@ -585,20 +585,20 @@ Int_t baseTreeDet::CutSamplebyID(Long64_t entry, Int_t myid)
     
     //CR1
     else if (myid==1){
-        if (CutCR1(entry)>0) return 1;
+        if (CutPreselection(entry)>0 && pidfsi<=0.08 && mumucl>0.6 && pmucl>0.25) return 1;
         else return -1;
     }
     //CR2
     else if (myid==2){
         //if (CutCR23Comb(entry)>0) return 1;
-        if (CutPreselection(entry)>0 && pidfsi<0.0 && pidfsi1prescor>0.1) return 1;
+        if (CutPreselection(entry)>0 && pidfsi<=0.08 && mumucl>0.6 && pmucl<=0.25) return 1;
 	else return -1;
     }
     
    
      else if (myid==3){
         //if (CutCR23Comb(entry)>0) return 1;
-        if (CutPreselection(entry)>0 && pidfsi<0.0 && pidfsiccqecor>0.1 ) return 1;
+        if (CutPreselection(entry)>0 && pidfsi<=0.08 && mumucl<=0.6) return 1;
 	else return -1;
     } 
     
@@ -678,13 +678,13 @@ Int_t    baseTreeDet::GetEventType(Long64_t entry){
     
 }
 Int_t    baseTreeDet::GetSampleType(Long64_t entry){
-    if (CutSignalR(entry)>0) return 0;
+    if (CutPreselection(entry)>0 && pidfsi>0.08 && mumucl>0.6 && pmucl>0.25) return 0;
     
-    else if (CutCR1(entry)>0) return 1;
+    else if (CutPreselection(entry)>0 && pidfsi<=0.08 && mumucl>0.6 && pmucl>0.25) return 1;
     
-    else if (CutPreselection(entry)>0 && pidfsi<0.0 && pidfsi1prescor>0.1) return 2;
+    else if (CutPreselection(entry)>0 && pidfsi<=0.08 && mumucl>0.6 && pmucl<=0.25) return 2;
    
-	else if (CutPreselection(entry)>0 && pidfsi<0.0 && pidfsiccqecor>0.1) return 3; 
+    else if (CutPreselection(entry)>0 && pidfsi<=0.08 && mumucl<=0.6) return 3; 
     else return -1;
     
 }
